985d1bcdee941ec36a64e1435507c9b87847d505
David Tabor <1davidtabor1@gmail.com>
2022-04-15 00:31:41 -0700

    Created main window

--- main.py
+++ main.py
@@ -4,3 +4,17 @@
     CIT 128: Python Programming II
     Student Directed Project
 '''
+import tkinter as tk
+
+# Main app window
+class LayoutGenerator(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.title = 'Layout Generator'
+        self.labeltext = 'Hello World'
+        self.greeting = tk.Label(self, text=self.labeltext)
+        self.greeting.pack()
+
+
+a = LayoutGenerator()
+a.mainloop()
