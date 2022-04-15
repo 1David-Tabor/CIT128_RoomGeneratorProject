9a05b031133370888d57967bf00de5e7ff852a80
David Tabor <1davidtabor1@gmail.com>
2022-04-15 00:40:29 -0700

    Added window geometry based on user screen.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
     Student Directed Project
 '''
 import tkinter as tk
+from turtle import screensize
 
 # Main app window
 class LayoutGenerator(tk.Tk):
@@ -12,8 +13,14 @@ class LayoutGenerator(tk.Tk):
         tk.Tk.__init__(self)
         self.title = 'Layout Generator'
         self.labeltext = 'Hello World'
+        winSizeMulti = 0.25 # 0.25 for 1/4 screen, 0.5 for 1/2 screen, etc.
+        self.screen_width = (int(self.winfo_screenwidth() * winSizeMulti))
+        self.screen_height = (int(self.winfo_screenheight() * winSizeMulti))
+        self.geometry(f"{self.screen_width}x{self.screen_height}")
+
         self.greeting = tk.Label(self, text=self.labeltext)
         self.greeting.pack()
+
 
 
 a = LayoutGenerator()
