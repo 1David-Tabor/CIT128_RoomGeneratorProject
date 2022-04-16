eeb36bb789923d0b8a5d2beabdb8a277212d8f21
David Tabor <1davidtabor1@gmail.com>
2022-04-16 00:26:12 -0700

    Added tabs

--- main.py
+++ main.py
@@ -5,8 +5,9 @@
     Student Directed Project
 '''
 import tkinter as tk
+from tkinter import ttk, Frame
 
-class Room():
+class Room:
     def __init__(self, roomSize, doorLocations):
         self.roomSize = roomSize
         self.doorLocations = doorLocations
@@ -23,11 +24,26 @@ class LayoutGenerator(tk.Tk):
         self.geometry(f"{self.screen_width}x{self.screen_height}")
 
         self.greeting = tk.Label(self, text=self.labeltext)
-        self.greeting.pack()
+        #self.greeting.pack()
+
+        #Creating tabs for input, output, and help page.
+        tabs = ttk.Notebook(self)
+        tabs.pack()
+        
+        frame1 = Frame(tabs, width=self.screen_width, height=self.screen_width)
+        frame1.pack(fill='both', expand=True)
+        frame2 = Frame(tabs, width=self.screen_width, height=self.screen_width)
+        frame2.pack(fill='both', expand=True)
+        frame3 = Frame(tabs, width=self.screen_width, height=self.screen_width)
+        frame3.pack(fill='both', expand=True)
+
+        tabs.add(frame1, text='Input')
+        tabs.add(frame2, text='Output')
+        tabs.add(frame3, text='Help')
+
 
 
 
 a = LayoutGenerator()
 a.mainloop()
 
-#Testing permutation
